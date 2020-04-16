861a457ac35ef9a29f3c78d9e4d6270c97616346
Vlad <iordanvlad36@gmail.com>
2020-04-16 09:18:58 +0300

    lab 7 - CameraActivity

--- Androidlab2/app/src/main/java/com/example/androidlab2/MainActivity.java
+++ Androidlab2/app/src/main/java/com/example/androidlab2/MainActivity.java
@@ -71,11 +71,16 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
            Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
            startActivity(intent);
            return true;
-       }else if (id == R.id.action_sensors) {
+       } else if (id == R.id.action_sensors) {
            Intent intent = new Intent(MainActivity.this, SensorsActivity.class);
            startActivity(intent);
            return true;
-       }
+       } else if (id == R.id.action_camera) {
+            Intent intent = new Intent(MainActivity.this, CameraActivity.class);
+            startActivity(intent);
+            return true;
+        }
+
        return super.onOptionsItemSelected(item);
     }
 
